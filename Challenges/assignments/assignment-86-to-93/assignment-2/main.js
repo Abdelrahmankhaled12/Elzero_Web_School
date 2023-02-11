1103de688d4b66272fbf362d747b9b98c1a14e4d
Abdelrahman khaled <108089479+Abdelrahmankhaled12@users.noreply.github.com>
2023-02-11 20:02:35 +0200

    Delete Challenges directory

--- Challenges/assignments/assignment-86-to-93/assignment-2/main.js
+++
@@ -1,8 +0,0 @@
-let images = document.querySelectorAll("img");
-
-for(let i = 0 ; i < images.length ; i++) {
-    images[i].setAttribute("src","https://elzero.org/wp-content/themes/elzero/imgs/logo.png");
-    images[i].setAttribute("alt","Elzero Logo");
-}
-
-
--- Challenges/assignments/assignment-86-to-93/assignment-3/main.js
+++
@@ -1,8 +0,0 @@
-
-let inputValue = document.getElementsByTagName("input")[0];
-let result = document.querySelector("div");
-
-inputValue.oninput = ()=> {
-        let value = inputValue.value;
-        result.textContent = `{${value}} USD Dollar = {${(value* 15.6).toFixed(2)}} Egyptian Pound`
-}
